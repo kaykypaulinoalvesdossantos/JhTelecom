34a2d69992ee2e2ca08d5405d3689e8e11b7d668
kaykypaulinoalvesdossantos <kayky.paulinoalves@gmail.com>
2023-11-16 08:50:37 -0300

    Conteudo

--- src/components/Sobre/mission/Mission.tsx
+++ src/components/Sobre/mission/Mission.tsx
@@ -3,11 +3,11 @@ export default function Massion(){
         <div className="flex justify-around max-md:flex-col max-md:gap-14">
             <div className="space-y-7 text-center max-md:flex max-md:items-center max-md:flex-col">
                 <h3 className="font-montserrat text-5xl font-bold max-md:text-3xl">Missão</h3>
-                <p className="max-w-xl text-xl text-center max-md:text-lg max-sm:text-base">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <p className="max-w-xl text-xl text-center max-md:text-lg max-sm:text-base">Comprometemo-nos a proporcionar serviços de alta qualidade em Telecomunicações, superando as expectativas e satisfazendo as necessidades específicas de nossos clientes. Nosso empenho reside em estabelecer conexões confiáveis e duradouras, impulsionando o sucesso através da excelência operacional.</p>
             </div>
             <div className="space-y-7 text-center max-md:flex max-md:items-center max-md:flex-col" >
                 <h3 className="font-montserrat text-5xl font-bold max-md:text-3xl">Visão</h3>
-                <p className="max-w-xl text-xl text-center max-md:text-lg max-sm:text-base">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <p className="max-w-xl text-xl text-center max-md:text-lg max-sm:text-base">Almejamos ser reconhecidos como líderes na prestação de serviços em Telecomunicações, destacando-nos pela qualidade e excelência em cada projeto. Buscamos constantemente inovações que elevem os padrões do setor, consolidando nossa posição como referência e inspirando confiança em nossos clientes e parceiros.</p>
             </div>
         </div>
     )
