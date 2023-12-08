b63abafa23a41d842822859bcc7a014aca813bc4
Kayky <kayky.paulinoalves@gmail.com>
2023-12-08 16:37:16 -0300

    Slid

--- src/components/home/compontsVideo/compontsVideo.tsx
+++ src/components/home/compontsVideo/compontsVideo.tsx
@@ -1,10 +1,46 @@
-import Image from "next/image";
-import Video from '@/../public/img/Video.png'
+'use client'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react'
 
-export default function CompontsVideo(){
-    return(
-        <div>
-            <Image src={Video} alt={""} className="w-full"/>
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+import './styles.css'
+
+// import required modules
+import Image from 'next/image'
+import { apressetation } from '@/mock/apressetation'
+import { Autoplay, Pagination } from 'swiper/modules'
+
+export default function CompontsVideo() {
+  return (
+    <>
+      <div className="relative">
+        <div className="absolute left-1/2 top-1/2 z-10 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center">
         </div>
-    )
+        <Swiper
+          spaceBetween={10}
+          loop={true}
+          autoplay={{
+            delay: 4000,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Autoplay, Pagination]}
+        >
+          {apressetation.map((IMG, Index) => (
+            <SwiperSlide
+              key={IMG.id}
+              className="!z-0 !max-w-full !h-[695px] object-cover text-white max-lg:!h-[495px] max-md:!h-[300px] max-sm:!h-[180px]"
+            >
+              <Image src={IMG.dir} alt="" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </>
+  )
 }
---
+++ src/mock/apressetation.ts
@@ -0,0 +1,28 @@
+import slide1 from '@/../public/img/Group 1.png'
+import slide2 from '@/../public/img/Group 87.png'
+import slide3 from '@/../public/img/Group 88.png'
+import slide4 from '@/../public/img/Group 89.png'
+import slide5 from '@/../public/img/Group 90.png'
+
+export const apressetation = [
+    {
+      id: 1,
+      dir: slide1,
+    },
+    {
+      id: 2,
+      dir: slide2,
+    },
+    {
+      id: 3,
+      dir: slide3,
+    },
+    {
+      id: 4,
+      dir: slide4,
+    },
+    {
+      id: 5,
+      dir: slide5,
+    },
+  ]
