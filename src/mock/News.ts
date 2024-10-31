8d451c1e9342e64d0d089ec5abce7087a030c7cd
kayky paulino <98046923+kaykypaulinoalvesdossantos@users.noreply.github.com>
2024-10-31 14:40:13 -0300

    Update News.ts

--- src/mock/News.ts
+++ src/mock/News.ts
@@ -1,16 +1,16 @@
-import Noticia from "@/../public/img/Servicesimg02.png";
+import vaga from "@/../public/img/vaga.jpg";
 import Noticia1 from "@/../public/img/Contratação.png";
 import Noticia2 from "@/../public/img/futebol-1lugar.jpeg";
 import fute from "@/../public/img/fute.png";
 export const News = [
     {
-      id: 1,
-      img: Noticia1,
-      title: "Processo seletivo",
-      text:
-        "JH TELECOM abre processo seletivo para técnicos instaladores em Fibra óptica, envie o seu currículo marcosdemetrio@jhtele.com.br",
-      date: "28/02/2024",
-      slug: "Teste01",
+    id: 1,
+    img: vaga,
+    title: "Estamos contratando novos tecnicos para nosso time de intalação"",
+    text:
+      "A JH Telecom está contratando novos técnicos para instalação de fibra óptica! Venha fazer parte de uma equipe dinâmica e cresça na área de telecomunicações com excelentes ganhos, além de benefícios como ajuda de custo para combustível e aluguel de veículo, entre outros. Não perca essa oportunidade de impulsionar sua carreira!, envie o seu currículo marcosdemetrio@jhtele.com.br",
+    date: "31/10/2024",
+    slug: "Teste01",
     },
     {
       id: 2,
